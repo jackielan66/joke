2381e5ef521b9fe55ec1b6036997e5ffc419a604
lhy <137067250@qq.com>
2020-05-19 21:43:35 +0800

    cross-env

--- src/utils/feach/index.js
+++ src/utils/feach/index.js
@@ -1,17 +1,19 @@
 // 各种采集入口
-const {startGet_163_Text} = require('./163')
+const { startGet_163_Text } = require('./163')
 
 
-function startGetText(){
+function startGetText() {
     startGet_163_Text();
 }
 
-
+// let stri  = typeof process.env.NODE_ENV;
+// console.log(stri,'stri')
+// console.log(process.env.NODE_ENV, 'process.env.NODE_ENV != "production"')
 let second = 6000;
-if (process.env.NODE_ENV == 'prodution') {
+if (process.env.NODE_ENV === 'production') {
+    console.log('production')
     second = 6000 * 60 * 6
 }
-second = 6000 * 60 * 6;
 const startCollect = function () {
     startGetText();
     setInterval(() => {
